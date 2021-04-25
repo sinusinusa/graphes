071752020e71096db25e29290387c0ebe8743483
Nikita Lysikov <55156606+sinusinusa@users.noreply.github.com>
2021-04-26 00:11:33 +0300

    Add files via upload

---
+++ task5graphics/Form1.Designer.cs
@@ -0,0 +1,369 @@
+﻿namespace task5graphics
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.create = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.red = new System.Windows.Forms.CheckBox();
+            this.blue = new System.Windows.Forms.CheckBox();
+            this.black = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.sX = new System.Windows.Forms.Label();
+            this.sY = new System.Windows.Forms.Label();
+            this.sColor = new System.Windows.Forms.Label();
+            this.change = new System.Windows.Forms.Button();
+            this.sA = new System.Windows.Forms.Label();
+            this.sB = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.delete = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.sNodes = new System.Windows.Forms.Label();
+            this.sArcs = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.changeA = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(921, 550);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // create
+            // 
+            this.create.AutoSize = true;
+            this.create.Location = new System.Drawing.Point(955, 196);
+            this.create.Name = "create";
+            this.create.Size = new System.Drawing.Size(82, 21);
+            this.create.TabIndex = 2;
+            this.create.Text = "создать";
+            this.create.UseVisualStyleBackColor = true;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // red
+            // 
+            this.red.AutoSize = true;
+            this.red.Location = new System.Drawing.Point(956, 24);
+            this.red.Name = "red";
+            this.red.Size = new System.Drawing.Size(86, 21);
+            this.red.TabIndex = 4;
+            this.red.Text = "красный";
+            this.red.UseVisualStyleBackColor = true;
+            this.red.Click += new System.EventHandler(this.red_Click);
+            // 
+            // blue
+            // 
+            this.blue.AutoSize = true;
+            this.blue.Location = new System.Drawing.Point(956, 51);
+            this.blue.Name = "blue";
+            this.blue.Size = new System.Drawing.Size(69, 21);
+            this.blue.TabIndex = 5;
+            this.blue.Text = "синий";
+            this.blue.UseVisualStyleBackColor = true;
+            this.blue.Click += new System.EventHandler(this.blue_Click);
+            // 
+            // black
+            // 
+            this.black.AutoSize = true;
+            this.black.Location = new System.Drawing.Point(956, 78);
+            this.black.Name = "black";
+            this.black.Size = new System.Drawing.Size(80, 21);
+            this.black.TabIndex = 6;
+            this.black.Text = "чёрный";
+            this.black.UseVisualStyleBackColor = true;
+            this.black.Click += new System.EventHandler(this.black_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(926, 223);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "соеденить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sX
+            // 
+            this.sX.AutoSize = true;
+            this.sX.Location = new System.Drawing.Point(955, 265);
+            this.sX.Name = "sX";
+            this.sX.Size = new System.Drawing.Size(14, 17);
+            this.sX.TabIndex = 8;
+            this.sX.Text = "x";
+            // 
+            // sY
+            // 
+            this.sY.AutoSize = true;
+            this.sY.Location = new System.Drawing.Point(955, 282);
+            this.sY.Name = "sY";
+            this.sY.Size = new System.Drawing.Size(15, 17);
+            this.sY.TabIndex = 9;
+            this.sY.Text = "y";
+            // 
+            // sColor
+            // 
+            this.sColor.AutoSize = true;
+            this.sColor.Location = new System.Drawing.Point(955, 299);
+            this.sColor.Name = "sColor";
+            this.sColor.Size = new System.Drawing.Size(39, 17);
+            this.sColor.TabIndex = 10;
+            this.sColor.Text = "color";
+            // 
+            // change
+            // 
+            this.change.Location = new System.Drawing.Point(927, 319);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(137, 23);
+            this.change.TabIndex = 11;
+            this.change.Text = "изменить узел";
+            this.change.UseVisualStyleBackColor = true;
+            this.change.Click += new System.EventHandler(this.change_Click);
+            // 
+            // sA
+            // 
+            this.sA.AutoSize = true;
+            this.sA.Location = new System.Drawing.Point(1019, 265);
+            this.sA.Name = "sA";
+            this.sA.Size = new System.Drawing.Size(17, 17);
+            this.sA.TabIndex = 12;
+            this.sA.Text = "A";
+            // 
+            // sB
+            // 
+            this.sB.AutoSize = true;
+            this.sB.Location = new System.Drawing.Point(1020, 282);
+            this.sB.Name = "sB";
+            this.sB.Size = new System.Drawing.Size(17, 17);
+            this.sB.TabIndex = 13;
+            this.sB.Text = "B";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(923, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 17);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "x =";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(923, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "y =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(1045, 265);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "= IndexA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1045, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 17);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "= IndexB";
+            // 
+            // delete
+            // 
+            this.delete.Location = new System.Drawing.Point(927, 348);
+            this.delete.Name = "delete";
+            this.delete.Size = new System.Drawing.Size(137, 23);
+            this.delete.TabIndex = 18;
+            this.delete.Text = "удалить узел";
+            this.delete.UseVisualStyleBackColor = true;
+            this.delete.Click += new System.EventHandler(this.delete_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(946, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(152, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "сбросить выбор";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // sNodes
+            // 
+            this.sNodes.AutoSize = true;
+            this.sNodes.Location = new System.Drawing.Point(943, 418);
+            this.sNodes.Name = "sNodes";
+            this.sNodes.Size = new System.Drawing.Size(17, 17);
+            this.sNodes.TabIndex = 20;
+            this.sNodes.Text = "A";
+            // 
+            // sArcs
+            // 
+            this.sArcs.AutoSize = true;
+            this.sArcs.Location = new System.Drawing.Point(943, 435);
+            this.sArcs.Name = "sArcs";
+            this.sArcs.Size = new System.Drawing.Size(17, 17);
+            this.sArcs.TabIndex = 21;
+            this.sArcs.Text = "B";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(966, 418);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 17);
+            this.label5.TabIndex = 22;
+            this.label5.Text = "nodes";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(966, 435);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 17);
+            this.label6.TabIndex = 23;
+            this.label6.Text = "arcs";
+            // 
+            // changeA
+            // 
+            this.changeA.Location = new System.Drawing.Point(927, 377);
+            this.changeA.Name = "changeA";
+            this.changeA.Size = new System.Drawing.Size(137, 23);
+            this.changeA.TabIndex = 24;
+            this.changeA.Text = "изменить дугу";
+            this.changeA.UseVisualStyleBackColor = true;
+            this.changeA.Click += new System.EventHandler(this.changeA_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1026, 223);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 23);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "разъеденить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.ClientSize = new System.Drawing.Size(1144, 574);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.changeA);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sArcs);
+            this.Controls.Add(this.sNodes);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.delete);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sB);
+            this.Controls.Add(this.sA);
+            this.Controls.Add(this.change);
+            this.Controls.Add(this.sColor);
+            this.Controls.Add(this.sY);
+            this.Controls.Add(this.sX);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.black);
+            this.Controls.Add(this.blue);
+            this.Controls.Add(this.red);
+            this.Controls.Add(this.create);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox create;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.CheckBox red;
+        private System.Windows.Forms.CheckBox blue;
+        private System.Windows.Forms.CheckBox black;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label sX;
+        private System.Windows.Forms.Label sY;
+        private System.Windows.Forms.Label sColor;
+        private System.Windows.Forms.Button change;
+        private System.Windows.Forms.Label sA;
+        private System.Windows.Forms.Label sB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button delete;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label sNodes;
+        private System.Windows.Forms.Label sArcs;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button changeA;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ task5graphics/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task5graphics
+{
+    public partial class Form1 : Form
+    {
+        public class Node
+        {    
+            public PointF Pos { get; set; }
+            public Color Clr { get; set; }
+            public Node (PointF A, Color B)
+            {
+                Pos = A;
+                Clr = B;
+            }
+            public bool isSelected(PointF A)
+            {
+                bool yes = false;
+                if ((A.X <= Pos.X + 5 && A.X >= Pos.X - 5) && (A.Y <= Pos.Y + 5 && A.Y >= Pos.Y - 5)) {
+                    Controller.LastSelNode = Controller.SelNode;
+                    yes = true; 
+                }
+                return yes;
+            }
+        }
+        public class Arc
+        {
+            public int A { get; set; }
+            public int B { get; set; }
+            public PointF Mid  { get; set; }
+            public Color Clr { get; set; }
+            public Arc(int X, int Y, Color C)
+            {
+                int delt = -10;
+                if (Controller.nodes[A].Pos.X > 250 || Controller.nodes[B].Pos.X > 250) delt = 10;
+                A = X;
+                B = Y;
+                Clr = C;
+                Mid = new PointF((Controller.nodes[A].Pos.X + Controller.nodes[B].Pos.X) / 2 + delt, (Controller.nodes[A].Pos.Y + Controller.nodes[B].Pos.Y) / 2 + delt);
+            }
+
+        }
+        public static class Controller
+        {
+            public static Color SelColor;
+            public static PointF Click { get; set; }
+            public static PointF HoldUp { get; set; }
+            public static int SelNode { get; set; }
+            public static int LastSelNode { get; set; }
+            public static List<Node> nodes = new List<Node>();
+            public static List<Arc> arcs = new List<Arc>();
+            public static void changeN()
+            {
+                nodes[SelNode].Pos = Click;
+                nodes[SelNode].Clr = SelColor;
+            }
+            public static void changeA()
+            {
+                for (int i = 0; i < arcs.Count; i++)
+                {
+                    if ((arcs[i].A == SelNode && arcs[i].B == LastSelNode) || (arcs[i].B == SelNode && arcs[i].A == LastSelNode))
+                    {
+                        arcs[i].Clr = SelColor;
+                        arcs[i].Mid = Click;
+                    }
+                }
+            }
+            public static void addN()
+            {
+                bool isEmpty = true;
+                for (int i = 0; i < nodes.Count; i++)
+                {
+                    if (nodes[i].isSelected(Click)) isEmpty = false;
+                }
+               if(isEmpty == true) nodes.Add(new Node(Click, SelColor));
+            }
+            public static void delA()
+            {
+                for (int i = 0; i < arcs.Count; i++)
+                {
+                    if ((arcs[i].A == SelNode && arcs[i].B == LastSelNode) || (arcs[i].B == SelNode && arcs[i].A == LastSelNode))
+                    {
+                        arcs.Remove(arcs[i]);
+                    }
+                }
+            }
+            public static void delN()
+            {
+                int ind = SelNode;
+                for (int i = 0; i < arcs.Count; i++)
+                {
+                    if ((arcs[i].A == SelNode) || (arcs[i].B == SelNode))
+                    {
+                        arcs.Remove(arcs[i]);
+                    }
+                    if (arcs[i].A > SelNode) arcs[i].A -= 1;
+                    if (arcs[i].B > SelNode) arcs[i].B -= 1;
+                }
+                if (SelNode != -1)
+                {  
+                    nodes.Remove(nodes[SelNode]);
+                    SelNode = -1;
+                }
+            }
+            public static void addA()
+            {
+                if(SelNode != -1 && LastSelNode != -1)arcs.Add(new Arc(SelNode, LastSelNode, SelColor));
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            timer2.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            int h = Screen.PrimaryScreen.WorkingArea.Height;
+            int w = Screen.PrimaryScreen.WorkingArea.Width;
+            
+            PointF A = this.PointToClient(Panel.MousePosition);
+            Controller.Click = A;
+            if (create.Checked) { Controller.addN(); }
+            else
+            {
+                for(int i =0; i<Controller.nodes.Count; i++)
+                {
+                    if (Controller.nodes[i].isSelected(A)) Controller.SelNode = i;
+                }
+
+            }
+
+        }
+        public void ReDraw(List <Node> A, List <Arc> B)
+        {
+            
+            Graphics g = pictureBox1.CreateGraphics();
+
+                for (int i = 0; i < A.Count; i++)
+                {
+                Pen c = new Pen(Color.OrangeRed);
+                Pen b = new Pen(A[i].Clr);
+                g.DrawEllipse(b, A[i].Pos.X-5, A[i].Pos.Y-5, 10, 10);
+                if(i == Controller.SelNode || i == Controller.LastSelNode)
+                {
+                    g.DrawEllipse(b, A[i].Pos.X - 3, A[i].Pos.Y - 3, 6, 6);
+                }
+                if (i == Controller.SelNode)
+                {
+                    g.DrawEllipse(c, A[i].Pos.X - 3, A[i].Pos.Y - 3, 6, 6);
+                }
+            }
+            for (int i = 0; i < B.Count; i++)
+            {
+                Pen b = new Pen(B[i].Clr);
+                PointF X = A[B[i].A].Pos;
+                PointF Y = A[B[i].B].Pos;
+                PointF M = B[i].Mid;
+                PointF[] mass = new PointF[3] { X, M, Y };
+                g.DrawCurve(b, mass);
+            }
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ReDraw(Controller.nodes, Controller.arcs);
+            sX.Text = Convert.ToString(Controller.Click.X);
+            sY.Text = Convert.ToString(Controller.Click.Y);
+            sColor.Text = Convert.ToString(Controller.SelColor);
+            sA.Text = Convert.ToString(Controller.SelNode);
+            sB.Text = Convert.ToString(Controller.LastSelNode);
+            sNodes.Text = Convert.ToString(Controller.nodes.Count);
+            sArcs.Text = Convert.ToString(Controller.arcs.Count);
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timer2.Interval = 10 * timer1.Interval;
+            Graphics g = pictureBox1.CreateGraphics();
+            g.Clear(Color.White);
+        }
+
+        private void black_Click(object sender, EventArgs e)
+        {
+            blue.Checked = false;
+            red.Checked = false;
+            Controller.SelColor = Color.Black;
+            black.Checked = true;
+        }
+
+        private void blue_Click(object sender, EventArgs e)
+        {
+            red.Checked = false;
+            black.Checked = false;
+            Controller.SelColor = Color.Blue;
+            blue.Checked = true;
+        }
+
+        private void red_Click(object sender, EventArgs e)
+        {
+            blue.Checked = false;
+            black.Checked = false;
+            Controller.SelColor = Color.Red;
+            red.Checked = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Controller.addA();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Controller.SelNode = -1;
+            Controller.LastSelNode = -1;
+        }
+
+        private void delete_Click(object sender, EventArgs e)
+        {
+            Controller.delN();
+        }
+
+        private void change_Click(object sender, EventArgs e)
+        {
+            Controller.changeN();
+        }
+
+        private void changeA_Click(object sender, EventArgs e)
+        {
+            Controller.changeA();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Controller.delA();
+        }
+    }
+}
